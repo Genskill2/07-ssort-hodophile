0df8f19502d16d85ddd80c3fb3249f78f01a2cdc
hodophile <71829615+hodophile@users.noreply.github.com>
2021-07-01 17:25:37 +0530

    Update swap.c

---
+++ swap.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void swap_max(int arr[],int n,int l){
+  int largest = -99999,pos,temp;
+  /*for(int i=0;i<l;i++){
+    printf("%i\t",arr[i]);
+  }
+  printf("\n");*/
+  for(int i=n;i<l;i++){
+    if(largest<arr[i]){
+      largest = arr[i];
+      pos=i;
+    }
+  }
+  temp = arr[n];
+  arr[n] = largest;
+  arr[pos] = temp;
+}
+
+void ssort(int arr[],int l){
+  for(int i=0;i<l;i++){
+    swap_max(arr,i,l);
+  }
+  /* for(int i=0;i<l;i++){
+    printf("%i\t",arr[i]);
+    }*/
+}
